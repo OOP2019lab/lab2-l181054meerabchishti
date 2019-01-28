6b1a4a3597c34a06b078a071c58df603d3e39ef4
l181054meerabchishti <46877062+l181054meerabchishti@users.noreply.github.com>
2019-01-28 10:43:59 +0500

    Add files via upload

---
+++ Q1_l181054.h.h
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+struct gitHubUser
+{
+
+	string firstName;
+	string userName;
+	string password;
+	string email;
+    int folderCount;
+
+
+};
